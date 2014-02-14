475a8a02c93e13a9153de878f836a8884689f8cc
ab24v07 <ash.booth@soton.ac.uk>
2014-02-14 23:03:40 +0000

    Initil commit from local git

---
+++ cpp-algos/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cpp-algos
+//
+//  Created by Ash Booth on 14/02/2014.
+//  Copyright (c) 2014 Ash Booth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
